1b3a468ec96aaa42be755f4c50afce85e8c376eb
Sigh-rill <xyrilleenriquemagno@gmail.com>
2025-11-14 12:34:14 -0500

    image changing function done

--- allScripts/introScript.js
+++ allScripts/introScript.js
@@ -1,4 +1,5 @@
 'use strict';
+//Making the dialogue on the bottom of the screen change
 const dialogue = document.querySelector('p');
 const screenDialogue = [
     'Hi how you doin',
@@ -10,4 +11,17 @@ document.addEventListener('click', runDialogue);
 function runDialogue(e){
     dialogue.textContent = screenDialogue[i];
     i++;
+}
+//Making the image change on the screen change
+//Make sure images are the correct size for the screen
+const screen = document.getElementById('screen');
+const screenImages = [
+  "url('../assets/hollowknight.jpg')",
+  "url('../assets/hornet.jpg')",
+  "url('../assets/NeoVim.jpg')",
+  "url('../assets/windows.jpg')",
+];
+document.addEventListener('click', changeScreen);
+function changeScreen(e){
+    screen.style.backgroundImage = screenImages[i];
 }
