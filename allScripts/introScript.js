d60a5bd462f88448c66d1d2cc67a6ab1cfff5b01
Sigh-rill <xyrilleenriquemagno@gmail.com>
2025-11-14 13:07:08 -0500

    change img on dialogue end done

--- allScripts/introScript.js
+++ allScripts/introScript.js
@@ -1,16 +1,38 @@
 'use strict';
 //Making the dialogue on the bottom of the screen change
 const dialogue = document.querySelector('p');
+const startingDialogue = [
+  'starting first sentence',
+  'starting second sentence',
+  'starting third sentence'
+]
+const secondDialogue = [
+  'second first sentence',
+  'second second sentence',
+  'second third sentence'
+]
+const endingDialogue = [
+  'end first sentence',
+  'end second sentence',
+  'end third sentence'
+]
 const screenDialogue = [
-    'Hi how you doin',
-    'Im doing great too',
-    'Have you looked around yet?'
+    startingDialogue,
+    secondDialogue,
+    endingDialogue
 ];
 let i = 0;
+let j = 0;
 document.addEventListener('click', runDialogue);
 function runDialogue(e){
-    dialogue.textContent = screenDialogue[i];
-    i++;
+    let currentDialogue = screenDialogue[i];
+    dialogue.textContent = currentDialogue[j];
+    j++;
+    if (j > currentDialogue.length){
+      i++;
+      j = 0;
+      changeScreen();
+    }
 }
 //Making the image change on the screen change
 //Make sure images are the correct size for the screen
@@ -21,7 +43,7 @@ const screenImages = [
   "url('../assets/NeoVim.jpg')",
   "url('../assets/windows.jpg')",
 ];
-document.addEventListener('click', changeScreen);
-function changeScreen(e){
+function changeScreen(){
     screen.style.backgroundImage = screenImages[i];
 }
+//Making image change only after a certain set of dialogue finishes
