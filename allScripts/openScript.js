435510d45a0f848e2da1cdc3b7c6a6960fe8a5f6
Zevs108 <yevseienkovimp@gmail.com>
2025-11-18 11:50:09 -0500

    Did the main logic for progressing the game (text and image switching)

--- allScripts/openScript.js
+++ allScripts/openScript.js
@@ -1,21 +1,31 @@
 "use strict";
 const sceneInfo = {
-	1 : ["SOME TEXT IS HERE", "OTHER TEXT IS HERE", "AGAIN TEXT IS HERE"],
-	2 :
+	0 : ["0OTHER TEXT IS HERE", "0AGAIN TEXT IS HERE"],
+	1 : ["1SOME TEXT IS HERE", "1OTHER TEXT IS HERE", "1AGAIN TEXT IS HERE"],
+	2 : ["2SOME TEXT IS HERE", "2OTHER TEXT IS HERE", "2AGAIN TEXT IS HERE"],
+}
+
+const imageSrc = [
+	"../assets/hollowknight.jpg",
+	"../assets/hornet.jpg",
+	"../assets/logo.png"
+];
+
 const textTag = document.getElementById('text');
+const imageTag = document.getElementById('image');
+let countScene = 0;
+let countText = 0
 
-document.body.addEventListener('click', changeText);
+document.body.addEventListener('click', () => progressGame(event, textTag));
 
-let i = 0;
-function changeText(){
-	textTag.textContent = "";
 
-	if(text[i] === undefined){
-		i = 0;
-		j++;
+function progressGame(event,tag){
+	tag.textContent = "";
+	if (sceneInfo[countScene][countText] === undefined){
+		countScene++; countText = 0;
+		imageTag.style.backgroundImage = `url(${imageSrc[countScene]})`;
 	}
-
-	printText(textTag, text[i]); i++;
+	printText(tag, sceneInfo[countScene][countText]); countText++;
 }
 
 async function printText(tag, text){
