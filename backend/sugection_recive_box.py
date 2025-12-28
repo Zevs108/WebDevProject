c1d133256c55e9fda3ec47fd6df25b1d8c35424a
Tymur <mr.kanis82@gmail.com>
2025-12-27 22:51:38 -0500

    small changes

--- backend/sugection_recive_box.py
+++ backend/sugection_recive_box.py
@@ -5,14 +5,14 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
     json_string = json_file.read()
     json_file.close
 
-with open('txt.txt', 'w', encoding='utf-8') as txt_file:
+with open('storage.txt', 'w', encoding='utf-8') as txt_file:
     txt_file.write(json_string)
     txt_file.close
 
+# it is for "human like"
 # with open('data.json', 'r') as json_file:
 #     data = json.load(json_file)  # data is now a Python dictionary or list
 #     json_file.close
-
 
 # with open('txt.txt', 'w') as txt_file:
 #     # Example: if 'data' is a dictionary like {'name': 'John', 'age': 30}
